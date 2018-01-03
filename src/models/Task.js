95df18ee4a7344af860decd39127647e486c4438
Theodore <unawares15@gmail.com>
2018-01-03 13:04:32 +0600

    Active tasks actions

--- src/models/Task.js
+++ src/models/Task.js
@@ -1,16 +1,17 @@
-export default class Task {
-  /*
-    1) Task has original state which must be constant
-    2) Other fields may be changed
-    3) Task actions works with its fields and original state
-       Update action:
-              Gets fields and makes request to the server to change its values
-              Responsed task assigns to original, then other fields take values from it
-       Create action:
-              Get fields and makes request to the server to create a task
-              Responsed task assigns to original, then other fields take values from it
+/*
+  1) Task has original state which must be constant
+  2) Other fields may be changed
+  3) Task actions works with its fields and original state
+     Update action:
+            Gets fields and makes request to the server to change its values
+            Responsed task assigns to original, then other fields take values from it
+     Create action:
+            Get fields and makes request to the server to create a task
+            Responsed task assigns to original, then other fields take values from it
 
-  */
+*/
+
+export default class Task {
   get instance () {
     return this.instance
   }
